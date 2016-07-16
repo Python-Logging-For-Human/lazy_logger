8e35ae76bd09543e48e2aa83e58e5e0f58246b1b
tim <tim.yellow@gmail.com>
2016-07-16 14:32:29 +0800

    change interafce

--- ezlogging.py
+++ ezlogging.py
@@ -1,10 +1,38 @@
-def get_logger(): pass
+import logging
+
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOGGER_LEVEL = logging.DEBUG
+HANDLER_LEVEL = logging.DEBUG
 
 
-def get_stderr_logger(): pass
+def get_logger(name):
+    return logging.getLogger(name)
 
 
-def get_rotate_file_logger(): pass
+def log_to_console(logger,
+                      logger_level=LOGGER_LEVEL,
+                      handler_level=HANDLER_LEVEL,
+                      format=FORMAT):
+
+    logger.setLevel(logger_level)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(handler_level)
+
+    formatter = logging.Formatter(format)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
 
 
-def get_systemd_logger(): pass
+def log_to_rotated_file(logger): pass
+
+
+def log_to_systemd(logger): pass
+
+
+if __name__ == "__main__":
+    logger = get_logger(__name__)
+    logger = log_to_console(logger)
+    logger.debug('yo')
