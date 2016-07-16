71102297335687d9e090d5e1ede8d9d6e144fead
chairco <chairco@gmail.com>
2016-07-16 16:01:05 +0800

    use mocky path to use logging.Logger

--- program.py
+++ program.py
@@ -12,6 +12,32 @@ def _fake_print(*args, file=None, **kwargs):
         _real_print(*args, file=file, **kwargs)
 
 
+def monkeypatch_method(cls):
+    def decorator(func):
+        setattr(cls, func.__name__, func)
+        return func
+    return decorator
+
+
+@monkeypatch_method(logging.Logger)
+def patch(self, f):
+
+        @functools.wraps(f)
+        def patched(*args, **kwargs):
+            import builtins
+            builtins.print = _fake_print
+            try:
+                f(*args, **kwargs)
+            finally:
+                builtins.print = _real_print
+
+        return patched
+
+
+def get_logger():
+    return logging.getLogger(__name__)
+
+'''
 class Logger(logging.Logger):
     
     def __init__(self, *args, **kwargs):
@@ -29,10 +55,12 @@ class Logger(logging.Logger):
                 builtins.print = _real_print
 
         return patched
-
+'''
 
 logger = Logger('name')
 
+
+logger = get_logger()
 
 @logger.patch
 def main():
