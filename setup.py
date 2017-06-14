2d0fc6cb3b0a7e13a88e829681151d581d1b5f8f
chairco <chairco@gmail.com>
2017-06-15 03:43:57 +0800

    Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='lazy_logger',
-    version="0.1.1",
+    version="0.1.2",
     description='provide a better logging',
     long_description=open('README.md').read(),
     author='lazy_logger',
